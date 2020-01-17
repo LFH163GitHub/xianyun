2cfe2dc011390fb3e4dd9f535154e8b39409f183
LFH <LFH_chunshou@163.com>
2020-01-17 17:14:54 +0800

    解决警告：State的写法

--- store/user.js
+++ store/user.js
@@ -1,9 +1,14 @@
 //用户管理
-export const state = {
+// export const state = {
+//   // 采用接口返回的数据结构
+//   userInfo: {
+//   }
+// };
+//state 的写法
+export const state = () => ({
   // 采用接口返回的数据结构
-  userInfo: {
-  }
-};
+  userInfo: {}
+});
 
 export const mutations = {
   setUserInfo(state, data) {
