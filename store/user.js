845564835d1b17c9d759643a101932de3988f9bb
LFH <LFH_chunshou@163.com>
2020-02-08 18:53:40 +0800

    /国定的属性，异步修改state中的值的方法

--- store/user.js
+++ store/user.js
@@ -18,3 +18,22 @@ export const mutations = {
   //   //
   // }
 };
+
+//国定的属性，异步修改state中的值的方法
+export const actions = {
+  login(store, data) {
+    console.log(store);
+
+    //提交到登录接口r
+    return this.$axios({
+      url: "/accounts/login",
+      method: "POST",
+      data: data
+    }).then(res => {
+      let data = res.data;
+      // console.log(res.data);
+      //把数据储存到store中
+      store.commit("setUserInfo", data)
+    });
+  }
+};
