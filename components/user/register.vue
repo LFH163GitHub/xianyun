6263a423bc49720ea246bb5e172c1bc1d0452d7d
LFH <LFH_chunshou@163.com>
2020-02-09 10:59:37 +0800

    输入框的验证 以及发送验证码功能实现

--- components/user/register.vue
+++ components/user/register.vue
@@ -1,98 +1,158 @@
 <template>
-    <el-form
-        :model="form"
-        ref="form"
-        :rules="rules"
-        class="form">
-            <el-form-item class="form-item">
-                <el-input
-                placeholder="用户名手机">
-                </el-input>
-            </el-form-item>
+  <el-form :model="form" ref="form" :rules="rules" class="form">
+    <el-form-item class="form-item" prop="username">
+      <el-input v-model="form.username" placeholder="用户名手机"> </el-input>
+    </el-form-item>
 
-            <el-form-item class="form-item">
-                <el-input
-                placeholder="验证码" >
-                    <template slot="append">
-                        <el-button @click="handleSendCaptcha">
-                            发送验证码
-                        </el-button>
-                    </template>
-                </el-input>
-            </el-form-item>
+    <el-form-item class="form-item" prop="captcha">
+      <el-input v-model="form.captcha" placeholder="验证码">
+        <template slot="append">
+          <el-button @click="handleSendCaptcha">
+            发送验证码
+          </el-button>
+        </template>
+      </el-input>
+    </el-form-item>
 
-            <el-form-item class="form-item">
-                <el-input
-                placeholder="你的名字">
-                </el-input>
-            </el-form-item>
+    <el-form-item class="form-item" prop="nickname">
+      <el-input v-model="form.nickname" placeholder="你的名字"> </el-input>
+    </el-form-item>
 
-            <el-form-item class="form-item">
-                <el-input
-                placeholder="密码"
-                type="password"
-                ></el-input>
-            </el-form-item>
+    <el-form-item class="form-item" prop="password">
+      <el-input
+        v-model="form.password"
+        placeholder="密码"
+        type="password"
+      ></el-input>
+    </el-form-item>
 
-            <el-form-item class="form-item">
-                <el-input
-                placeholder="确认密码"
-                type="password">
-                </el-input>
-            </el-form-item>
+    <el-form-item class="form-item" prop="checkPassword">
+      <el-input
+        v-model="form.checkPassword"
+        placeholder="确认密码"
+        type="password"
+      >
+      </el-input>
+    </el-form-item>
 
-            <el-button
-            class="submit"
-            type="primary"
-            @click="handleRegSubmit">
-                注册
-            </el-button>
-        </el-form>
+    <el-button class="submit" type="primary" @click="handleRegSubmit">
+      注册
+    </el-button>
+  </el-form>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            // 表单数据
-            form: {},
-            // 表单规则
-            rules: {},
+  data() {
+    var validateUsername = (rule, value, callback) => {
+      let isVaild = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/.test(
+        value
+      );
+
+      if (!isVaild) {
+        callback(new Error("手机号码格式错误"));
+      } else {
+        callback();
+      }
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.form.password) {
+        callback(new Error("密码输入不一致！"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      // 表单数据
+      form: {
+        username: "",
+        password: "",
+        checkPassword: "",
+        nickname: "",
+        captcha: ""
+      },
+      // 表单规则
+      rules: {
+        username: [{ validator: validateUsername, trigger: "blur" }],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 3, message: "密码至少3位", trigger: "blur" }
+        ],
+        checkPassword: [{ validator: validatePass, trigger: "blur" }],
+        nickname: [{ required: true, message: "请输入昵称", trigger: "blur" }],
+        captcha: [{ required: true, message: "请输入验证码", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    // 发送验证码
+    handleSendCaptcha() {
+      if (!this.form.username) {
+        this.$confirm("手机号码不能为空", "提示", {
+          confirmButtonText: "确定",
+          showCancelButton: false,
+          type: "warning"
+        });
+        return;
+      }
+
+      if (
+        !/^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/.test(
+          this.form.username
+        )
+      ) {
+        this.$confirm("手机号码格式错误", "提示", {
+          confirmButtonText: "确定",
+          showCancelButton: false,
+          type: "warning"
+        });
+        return;
+      }
+
+      this.$axios({
+        url: `/captchas`,
+        method: "POST",
+        data: {
+          tel: this.form.username
         }
+      }).then(res => {
+        const { code } = res.data;
+        this.$confirm(`模拟手机验证码为：${code}`, "提示", {
+          confirmButtonText: "确定",
+          showCancelButton: false,
+          type: "warning"
+        });
+      });
     },
-    methods: {
-        // 发送验证码
-        handleSendCaptcha(){
 
-        },
-
-
-        // 注册
-        handleRegSubmit(){
-           console.log(this.form)
-        }
+    // 注册
+    handleRegSubmit() {
+      console.log(this.form);
     }
-}
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .form{
-        padding:25px;
-    }
+.form {
+  padding: 25px;
+}
 
-    .form-item{
-        margin-bottom:20px;
-    }
+.form-item {
+  margin-bottom: 20px;
+}
 
-    .form-text{
-        font-size:12px;
-        color:#409EFF;
-        text-align: right;
-        line-height: 1;
-    }
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
 
-    .submit{
-        width:100%;
-        margin-top:10px;
-    }
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
 </style>
---
+++ pages/tab.vue
@@ -0,0 +1,56 @@
+<template>
+  <div style="padding:100px">
+    <div class="btn">
+      <span
+        v-for="(item, index) in ['国际', '体育', '娱乐']"
+        :key="index"
+        :class="{ active: currt === index }"
+        @click="dian(index)"
+      >
+        {{ item }}
+      </span>
+    </div>
+    <div v-if="currt === 0">
+      <ul>
+        <li>hhh</li>
+        <li>hhh</li>
+        <li>hhh</li>
+      </ul>
+    </div>
+    <div v-if="currt === 1">
+      <ul>
+        <li>aaa</li>
+        <li>hhh</li>
+        <li>hhh</li>
+      </ul>
+    </div>
+    <div v-if="currt === 2">
+      <ul>
+        <li>bbb</li>
+        <li>hhh</li>
+        <li>hhh</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currt: 0
+    };
+  },
+  methods: {
+    dian(index){
+      this.currt =index
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.active {
+  color: red;
+}
+</style>
